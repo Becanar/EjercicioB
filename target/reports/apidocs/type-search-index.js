281d55ad7cc1b1e7c2353ef02f00e4fad0df2c71
Becanar <benatortubitarte@gmail.com>
2024-10-18 10:15:43 +0200

    Genero JavaDoc y cambio gitignore para que suba target

---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.ejerciciob","l":"Persona"},{"p":"org.example.ejerciciob","l":"Tabla"},{"p":"org.example.ejerciciob","l":"tablaController"}];updateSearchResults();
